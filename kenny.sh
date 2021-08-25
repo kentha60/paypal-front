cc43e06c1fbe75e6609e83ccdf6b250e2b4d3241
class25 <kenneth.tamuno@gmail.com>
2021-08-25 09:23:52 -0400

    battle rap

--- kenny.sh
+++ kenny.sh
@@ -1,3 +1,4 @@
 echo "this is for the brother up north"
 echo "sipping on that jack mark"
-echo "another smack bttle we got a battle nicca to back smack"
+echo "another smack bttle we got a battle nicca to back smaco 
+echo "Loaded Lux"
